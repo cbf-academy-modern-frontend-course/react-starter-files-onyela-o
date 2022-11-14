5ebd84a7fc1bfcb7878a058433313cc015c0b311
Onyela Ogah <83657534+onyela-o@users.noreply.github.com>
2022-11-14 13:07:03 +0000

    Add price, key to each headphone

--- props-and-state/src/components/GadgetList.js
+++ props-and-state/src/components/GadgetList.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React from 'react'
 
 const GadgetList = (props) => {
-
   return (
     <main>
       <h2>Headphones</h2>
-      {props.items.map((item) => (<div>{item.title}</div>))}
+      {props.items.map((item, index) => (
+        <div key={`headphone-product-${index}`}>
+          <div>{item.title}</div>
+          <div style={{ color: 'red' }}>{item.price}</div>
+        </div>
+      ))}
     </main>
-  );
+  )
 }
 
-export default GadgetList;
+export default GadgetList
--- props-and-state/src/index.js
+++ props-and-state/src/index.js
@@ -1,19 +1,27 @@
-import React, { useState, Fragment } from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState, Fragment } from 'react'
+import ReactDOM from 'react-dom/client'
 //import data from './models/headphones.json';
 
-import GadgetList from './components/GadgetList';
-const mainStyle = {"fontFamily":"Arial"};
+import GadgetList from './components/GadgetList'
+const mainStyle = { fontFamily: 'Arial' }
 
 const App = () => {
-  const headphones =  [{"title":"Headphones 1"},{"title":"Headphones 2"},{"title":"Headphones 3"},{"title":"Headphones 4"},];
-  const [headphonesList] = useState(headphones);
+  const headphones = [
+    { title: 'Headphones 1', price: '£340' },
+    { title: 'Headphones 2', price: '£120' },
+    { title: 'Headphones 3', price: '£80' },
+    { title: 'Headphones 4', price: '£150' },
+    { title: 'Headphones 5', price: '£20' },
+  ]
+  const [headphonesList] = useState(headphones)
 
-  return <section style={mainStyle}>
-    <h1>Electronics Store</h1>
-    <GadgetList items={headphonesList}/>
-  </section>;
+  return (
+    <section style={mainStyle}>
+      <h1>Electronics Store</h1>
+      <GadgetList items={headphonesList} />
+    </section>
+  )
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
