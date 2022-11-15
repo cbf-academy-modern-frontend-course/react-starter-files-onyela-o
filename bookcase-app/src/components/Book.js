3073582fa84e61f1cd4d5fcf3a40b8d45ab13e8e
Onyela Ogah <83657534+onyela-o@users.noreply.github.com>
2022-11-15 15:49:37 +0000

    Update img alternative text

--- bookcase-app/src/components/Book.js
+++ bookcase-app/src/components/Book.js
@@ -5,11 +5,15 @@ function Book(props) {
     <div>
       <h1>{props.title}</h1>
       <div>
-        by {props.authors?.length > 1 ?  <span>{props.authors.join(' & ')}</span> :
-        <span>{props.authors}</span>}
+        by{' '}
+        {props.authors?.length > 1 ? (
+          <span>{props.authors.join(' & ')}</span>
+        ) : (
+          <span>{props.authors}</span>
+        )}
       </div>
       {props.price ? <p>£{props.price}</p> : <p>No price</p>}
-      <img src={props.image} alt='Book cover'></img>
+      <img src={props.image} alt={props.title}></img>
       <br></br>
     </div>
   )
